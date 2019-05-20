0c53a9d854b6fbcda1334e2002571ebba07a0c5e
Alan Stern <stern@rowland.harvard.edu>
2019-05-20 10:44:21 -0400

    Synchronize codes for OnePlus 7 Oxygen OS 10.3.1/OnePlus 7 Pro Oxygen OS 10.3.1
    [CVE-2019-15220]p54usb: Fix race between disconnect and firmware loading
    
    The syzbot fuzzer found a bug in the p54 USB wireless driver.  The
    issue involves a race between disconnect and the firmware-loader
    callback routine, and it has several aspects.
    
    One big problem is that when the firmware can't be loaded, the
    callback routine tries to unbind the driver from the USB _device_ (by
    calling device_release_driver) instead of from the USB _interface_ to
    which it is actually bound (by calling usb_driver_release_interface).
    
    The race involves access to the private data structure.  The driver's
    disconnect handler waits for a completion that is signalled by the
    firmware-loader callback routine.  As soon as the completion is
    signalled, you have to assume that the private data structure may have
    been deallocated by the disconnect handler -- even if the firmware was
    loaded without errors.  However, the callback routine does access the
    private data several times after that point.
    
    Another problem is that, in order to ensure that the USB device
    structure hasn't been freed when the callback routine runs, the driver
    takes a reference to it.  This isn't good enough any more, because now
    that the callback routine calls usb_driver_release_interface, it has
    to ensure that the interface structure hasn't been freed.
    
    Finally, the driver takes an unnecessary reference to the USB device
    structure in the probe function and drops the reference in the
    disconnect handler.  This extra reference doesn't accomplish anything,
    because the USB core already guarantees that a device structure won't
    be deallocated while a driver is still bound to any of its interfaces.
    
    To fix these problems, this patch makes the following changes:
    
    	Call usb_driver_release_interface() rather than
    	device_release_driver().
    
    	Don't signal the completion until after the important
    	information has been copied out of the private data structure,
    	and don't refer to the private data at all thereafter.
    
    	Lock udev (the interface's parent) before unbinding the driver
    	instead of locking udev->parent.
    
    	During the firmware loading process, take a reference to the
    	USB interface instead of the USB device.
    
    	Don't take an unnecessary reference to the device during probe
    	(and then don't drop it during disconnect).
    
    Change-Id: I0cdfd9f1d875bb639af90a9eeb84d5dd435fa4e9
    Signed-off-by: Alan Stern <stern@rowland.harvard.edu>
    Reported-and-tested-by: chunkeey@gmail.com
    CC: <stable@vger.kernel.org>
    Acked-by: Christian Lamparter <chunkeey@gmail.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    (cherry picked from commit 6cf1d6c3e353f3c849890f7a80c9620619a61c0a)
    (cherry picked from commit d3cb04fbd61715f409b5615643dce58206d4f673)
    (cherry picked from commit 0bf2237c3b208e43b457e567769779750bbc1abf)

--- drivers/net/wireless/intersil/p54/p54usb.c
+++ drivers/net/wireless/intersil/p54/p54usb.c
@@ -911,10 +911,10 @@ err_stop:
 	p54u_stop(dev);
 
 err_out:
-	/*
-	 * p54u_disconnect will do the rest of the
-	 * cleanup
-	 */
+
+	/* p54u_disconnect will do the rest of the */
+	/* cleanup */
+
 	return ret;
 }
 
@@ -935,10 +935,10 @@ static void p54u_load_firmware_cb(const struct firmware *firmware,
 	}
 
 	complete(&priv->fw_wait_load);
-	/*
-	 * At this point p54u_disconnect may have already freed
-	 * the "priv" context. Do not use it anymore!
-	 */
+
+	 /* At this point p54u_disconnect may have already freed */
+	 /* the "priv" context. Do not use it anymore! */
+
 	priv = NULL;
 
 	if (err) {
