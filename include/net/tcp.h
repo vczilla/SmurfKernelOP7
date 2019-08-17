1b6a967787531ad6f0dcdae36b39af8ae585022e
Erik Müller <erik.lars.mueller@googlemail.com>
2019-08-17 23:26:51 +0200

    Fix Linux 4.14.138 merge

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -1650,11 +1650,6 @@ static inline void tcp_init_send_head(struct sock *sk)
 
 void tcp_write_queue_purge(struct sock *sk);
 
-static inline struct sk_buff *tcp_rtx_queue_head(const struct sock *sk)
-{
-	return skb_rb_first(&sk->tcp_rtx_queue);
-}
-
 static inline struct sk_buff *tcp_write_queue_head(const struct sock *sk)
 {
 	return skb_peek(&sk->sk_write_queue);
