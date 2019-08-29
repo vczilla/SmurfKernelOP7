0d7c45bcca40273fc9fed28894196fe2ea143155
Vinayak Menon <vinmenon@codeaurora.org>
2019-08-29 14:53:40 +0530

    simple_lmk: Update from SultanXDA and enable it again

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -4085,9 +4085,9 @@ static int balance_pgdat(pg_data_t *pgdat, int order, int classzone_idx)
 		unsigned long nr_reclaimed = sc.nr_reclaimed;
 		bool raise_priority = true;
 
-		sc.reclaim_idx = classzone_idx;
-
 		simple_lmk_decide_reclaim(sc.priority);
+
+		sc.reclaim_idx = classzone_idx;
 
 		/*
 		 * If the number of buffer_heads exceeds the maximum allowed
@@ -4244,7 +4244,6 @@ static void kswapd_try_to_sleep(pg_data_t *pgdat, int alloc_order, int reclaim_o
 	 * succeed.
 	 */
 	if (prepare_kswapd_sleep(pgdat, reclaim_order, classzone_idx)) {
-		simple_lmk_stop_reclaim();
 		/*
 		 * Compaction records what page blocks it recently failed to
 		 * isolate pages from and skips them in the future scanning.
@@ -4281,7 +4280,6 @@ static void kswapd_try_to_sleep(pg_data_t *pgdat, int alloc_order, int reclaim_o
 	 */
 	if (!remaining &&
 	    prepare_kswapd_sleep(pgdat, reclaim_order, classzone_idx)) {
-		simple_lmk_stop_reclaim();
 		trace_mm_vmscan_kswapd_sleep(pgdat->node_id);
 
 		/*
