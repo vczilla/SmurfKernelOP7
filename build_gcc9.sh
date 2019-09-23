c9a4d6c31ff6fe9eb82a3845e25be4e0552a8317
Arun KS <arunks@codeaurora.org>
2019-09-23 14:46:08 +0530

    Version 3.2.2

--- build_gcc9.sh
+++ build_gcc9.sh
@@ -23,7 +23,7 @@ export CROSS_COMPILE_ARM32=${HOME}/android/Toolchains/gcc9eabi_92/bin/arm-eabi-
 DEFCONFIG="smurf_defconfig"
 
 # Kernel Details
-VER=".3.2.1.unified"
+VER=".3.2.2.unified"
 
 # Paths
 KERNEL_DIR=`pwd`
