ed0568c2da0fdfe34c523a0d605a4ff9f3badeaa
Erik Müller <erik.lars.mueller@googlemail.com>
2019-10-18 19:05:26 +0200

    Version 3.1.6

--- build_gcc9.sh
+++ build_gcc9.sh
@@ -23,7 +23,7 @@ export CROSS_COMPILE_ARM32=${HOME}/android/Toolchains/gcc9eabi_92/bin/arm-eabi-
 DEFCONFIG="smurf_defconfig"
 
 # Kernel Details
-VER=".3.1.5.unified"
+VER=".3.1.6.unified"
 
 # Paths
 KERNEL_DIR=`pwd`
