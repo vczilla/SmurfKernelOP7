37ed7b22afd627625588563c420f749b2251d448
Erik Müller <erik.lars.mueller@googlemail.com>
2019-11-26 23:08:09 +0100

    houston: finalize fake driver

--- drivers/oneplus/coretech/houston/houston.c
+++ drivers/oneplus/coretech/houston/houston.c
@@ -1,9 +1,38 @@
 #define pr_fmt(fmt) "houston: " fmt
 
 #include <linux/init.h>
-#include <linux/moduleparam.h>
-
+#include <linux/kthread.h>
+#include <linux/module.h>
+#include <linux/tick.h>
+#include <linux/kernel.h>
+#include <linux/kernel_stat.h>
+#include <linux/delay.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/cpufreq.h>
+#include <linux/cpumask.h>
+#include <linux/freezer.h>
+#include <linux/wait.h>
+#include <linux/device.h>
+#include <linux/poll.h>
+#include <linux/ioctl.h>
+#include <linux/perf_event.h>
+#include <linux/cdev.h>
+#include <linux/workqueue.h>
+#include <linux/clk.h>
+#include <linux/oem/houston.h>
 #include <linux/oneplus/houston.h>
+
+struct cpuload_info {
+	int cnt;
+	int cmin;
+	int cmax;
+	int sum;
+	long long iowait_min;
+	long long iowait_max;
+	long long iowait_sum;
+};
+static struct ai_parcel parcel;
 
 static int log_lv = 0;
 module_param(log_lv, int, 0664);
@@ -62,6 +91,69 @@ module_param(sample_rate_ms, uint, 0664);
 
 unsigned int fps_data_sync = 0;
 module_param(fps_data_sync, uint, 0664);
+
+extern unsigned long long task_sched_runtime(struct task_struct *p);
+
+static long ht_ctl_ioctl(struct file *file, unsigned int cmd, unsigned long __user arg)
+{
+	if (_IOC_TYPE(cmd) != HT_IOC_MAGIC) return 0;
+	if (_IOC_NR(cmd) > HT_IOC_MAX) return 0;
+
+	switch (cmd) {
+	case HT_IOC_COLLECT:
+	{
+		memset (&parcel, 0, sizeof(parcel));
+		if (copy_to_user((struct ai_parcel __user *) arg, &parcel, sizeof(parcel)))
+			return 0;
+		break;
+	}
+	case HT_IOC_SCHEDSTAT:
+	{
+		struct task_struct *task;
+		u64 exec_ns = 0;
+		u64 pid;
+		if (copy_from_user(&pid, (u64 *) arg, sizeof(u64)))
+			return 0;
+
+		rcu_read_lock();
+		task = find_task_by_vpid(pid);
+		if (task) {
+			get_task_struct(task);
+			rcu_read_unlock();
+			exec_ns = task_sched_runtime(task);
+			put_task_struct(task);
+		} else {
+			rcu_read_unlock();
+		}
+		if (copy_to_user((u64 *) arg, &exec_ns, sizeof(u64)))
+			return 0;
+		break;
+	}
+	case HT_IOC_CPU_LOAD:
+	{
+		struct cpuload cl;
+		if (copy_from_user(&cl, (struct cpuload *) arg, sizeof(struct cpuload)))
+			return 0;
+		memset (&cl, 0, sizeof(struct cpuload));
+		if (copy_to_user((struct cpuload *) arg, &cl, sizeof(struct cpuload)))
+			return 0;
+		break;
+	}
+	}
+	return 0;
+}
+
+static unsigned int ht_ctl_poll(struct file *fp, poll_table *wait)
+{
+	return POLLIN;
+}
+
+static const struct file_operations ht_ctl_fops = {
+	.owner = THIS_MODULE,
+	.poll = ht_ctl_poll,
+	.unlocked_ioctl = ht_ctl_ioctl,
+	.compat_ioctl = ht_ctl_ioctl,
+};
 
 static int ht_init(void)
 {
