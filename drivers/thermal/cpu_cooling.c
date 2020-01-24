1864914127bec80b8631da08a3af82ea2195a7f8
Erik Müller <erik.lars.mueller@googlemail.com>
2020-01-24 01:01:55 +0100

    thermal: always return policy max

--- drivers/thermal/cpu_cooling.c
+++ drivers/thermal/cpu_cooling.c
@@ -332,7 +332,7 @@ static int cpufreq_thermal_notifier(struct notifier_block *nb,
 	 * the floor_frequency, then adjust the policy->min.
 	 */
 	if (policy->max > clipped_freq || policy->min < floor_freq)
-		cpufreq_verify_within_limits(policy, floor_freq, clipped_freq);
+		cpufreq_verify_within_limits(policy, policy->min, policy->max);
 	mutex_unlock(&cooling_list_lock);
 
 	return NOTIFY_OK;
