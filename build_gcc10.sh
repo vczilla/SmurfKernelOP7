341cbed0b4e7cbddfe17a0ea102dc47843d20531
pappschlumpf <erik.lars.mueller@googlemail.com>
2020-05-20 22:16:06 +0200

    Version 3.5.3

--- build_gcc10.sh
+++ build_gcc10.sh
@@ -23,7 +23,7 @@ export CROSS_COMPILE_ARM32=/media/${USER}/ExtremeExt4/Toolchains/gcc9eabi_92/bin
 DEFCONFIG="smurf_defconfig"
 
 # Kernel Details
-VER=".3.5.2.slmk.unified"
+VER=".3.5.3.slmk.unified"
 
 # Paths
 KERNEL_DIR=`pwd`
